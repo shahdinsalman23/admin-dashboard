604c71a1d6121d4a307d0ccec6c140988fac2e79
Shahdin Salman <125382694+shahdinsalman23@users.noreply.github.com>
2025-09-12 09:06:48 +0500

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,6 @@ import React from 'react'
 const Header = () => {
   return (
     <div className="header">
-        <h1>Header Component</h1>
-        <p>This is a placeholder for the header functionality.</p>
-        
     </div>
   )
 }
